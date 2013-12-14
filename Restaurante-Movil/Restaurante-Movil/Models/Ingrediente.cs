6ccf63437b63ec902e98283a92613f159c51323c
CarlosAlmonteIbarra <cfai.tiko@gmail.com>
2013-12-14 14:43:10 -0800

    Model - Ingrediente

---
+++ Restaurante-Movil/Restaurante-Movil/Models/Ingrediente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurante_Movil.Models
+{
+    public class Ingrediente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public short Tipo { get; set; }
+        public decimal Cantidad { get; set; }
+        public enum TipoIngrediente
+        {
+            ALCOHOL = 1,
+            AVE,
+            CARNE,
+            CONDIMENTO,
+            ESPECIA,
+            FRUTA,
+            GASEOSA,
+            GRANO,
+            JUGO,
+            LACTEO,
+            MARISCO,
+            TUBERCULO,
+            VERDURA
+        }
+        public static int AmountTipos { get { return 13; } }
+        public string StringTipoIngrediente { get; set; }
+
+    }
+
+}
