59678ec2704abd438259202933ccc13db04040a9
Martin Toledo <vizardespa@gmail.com>
2013-12-14 16:19:58 -0800

    Like a boss
    llmll

---
+++ Restaurante-Movil/Restaurante-Movil/Controllers/AgregarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurante_Movil.Models;
+
+namespace Restaurante_Movil.Controllers
+{
+    public class AgregarController : Controller
+    {
+        //
+        // GET: /Agregar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Ingrediente model)
+        {
+            // enviar model a Services
+            return View();
+        }
+    }
+}
