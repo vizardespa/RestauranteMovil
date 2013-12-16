264bdaeba7289994593f118f877f796ed848123e
Martin Toledo <vizardespa@gmail.com>
2013-12-15 19:47:29 -0800

    Agregado lo de Carlos

--- Restaurante-Movil/Restaurante-Movil/Controllers/HomeController.cs
+++ Restaurante-Movil/Restaurante-Movil/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurante_Movil.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,20 +12,36 @@ namespace Restaurante_Movil.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modifique esta plantilla para poner en marcha su aplicación ASP.NET MVC.";
-            List<Models.Ingrediente> list = GetIngredientes();
+            List<Ingrediente> list = GetIngredientes();
             ViewBag.table = ConvertToTable(list);
+            // Opciones para hacer busquedas por Tipo de Ingrediente.
+            List<SelectListItem> tipos_ingrediente = new List<SelectListItem>();
+            SetTiposIngrediente(tipos_ingrediente);
+            ViewBag.Tipos = tipos_ingrediente;
 
             return View();
         }
-        private System.Data.DataTable ConvertToTable(List<Models.Ingrediente> list)
+        [HttpPost]
+        public ActionResult Index(Consulta model)
+        {
+            List<Ingrediente> list = SearchIngredientes(model);
+            ViewBag.table = ConvertToTable(list);
+            // Opciones para hacer busquedas por Tipo de Ingrediente.
+            List<SelectListItem> tipos_ingrediente = new List<SelectListItem>();
+            SetTiposIngrediente(tipos_ingrediente);
+            ViewBag.Tipos = tipos_ingrediente;
+
+            return View();
+        }
+
+        private System.Data.DataTable ConvertToTable(List<Ingrediente> list)
         {
             System.Data.DataTable dt = new System.Data.DataTable();
             dt.Columns.Add("ID");
             dt.Columns.Add("Nombre");
             dt.Columns.Add("Tipo");
             dt.Columns.Add("Cantidad");
-            
+
             foreach (Models.Ingrediente i in list)
             {
                 dt.Rows.Add(new object[4]{
@@ -36,11 +53,62 @@ namespace Restaurante_Movil.Controllers
 
             return dt;
         }
-        public List<Models.Ingrediente> GetIngredientes()
+        private List<Ingrediente> GetIngredientes()
         {
-            List<Models.Ingrediente> list = new List<Models.Ingrediente>();
-            list.Add(new Models.Ingrediente { Id = 30, Nombre = "juguito", Tipo = 4, Cantidad = (decimal)320.25 });
+            List<Ingrediente> list = new List<Models.Ingrediente>();
+
+            // Consumir Service para obtener todos los ingredientes y guardarlos en List<>.
+            /*
+            var ingredientes = new Restaurante_Services().ServiceClient().ObtenersIngredientesAsync().Result;
+            foreach (var i in ingredientes)
+                list.Add(new Ingrediente
+                {
+                    Id = i.Id,
+                    Nombre = i.Nombre,
+                    Tipo = i.Tipo,
+                    StringTipoIngrediente = i.StringTipoIngrediente,
+                    Cantidad = i.Cantidad
+                });
+            */
             return list;
+        }
+        private List<Ingrediente> SearchIngredientes(Consulta model)
+        {
+            List<Ingrediente> list = new List<Ingrediente>();
+
+            object value;
+            int x;
+            if (model.by == 0 && int.TryParse(model.keyword, out x))
+                value = x;
+            else if (model.by == 2)
+                value = model.type;
+            else
+                value = model.keyword as string;
+
+            /*
+            var ingredientes = serviceclient.BuscarIngredientes(model.by, value).Result;
+            foreach (var i in ingredientes)
+                list.Add(new Ingrediente
+                {
+                    Id = i.Id,
+                    Nombre = i.Nombre,
+                    Tipo = i.Tipo,
+                    StringTipoIngrediente = i.StringTipoIngrediente,
+                    Cantidad = i.Cantidad
+                });
+            */
+            return list;
+        }
+        private void SetTiposIngrediente(List<SelectListItem> list)
+        {
+            for (int i = 0; i <= Ingrediente.AmountTipos; i++)
+            {
+                list.Add(new SelectListItem
+                {
+                    Value = i.ToString(),
+                    Text = ((Ingrediente.TipoIngrediente)i).ToString()
+                });
+            }
         }
 
         public ActionResult About()
